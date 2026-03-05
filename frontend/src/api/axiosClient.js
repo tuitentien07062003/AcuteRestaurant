cbb02f3f15ca61d26239d068bd4c1b698d94922e
tuitentien07062003 <nguyenphanquoctien07062003@gmail.com>
2026-03-05 13:58:26 +0700

    deploy

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000/acute",
+  baseURL: "https://acuterestaurant.onrender.com",
   withCredentials: true,
 });
 
