db3b7cb514a3a3067a9478296289e97ee961d57a
tuitentien07062003 <nguyenphanquoctien07062003@gmail.com>
2026-03-05 15:41:51 +0700

    v3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Toaster, toast } from 'sonner';
 import { BrowserRouter, Routes, Route } from 'react-router';
+import { GlobalProvider } from '@/context/GlobalContext';
 import Pos from './pages/Pos';
 import Admin from './pages/Admin';
 import Login from './pages/Login';
@@ -9,14 +10,16 @@ function App() {
   return (
     <>
       <Toaster richColors position='top-center'/>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/pos" element={<Pos />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="*" element={<Pos />} />
-        </Routes>
-      </BrowserRouter>
+      <GlobalProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/pos" element={<Pos />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="*" element={<Pos />} />
+          </Routes>
+        </BrowserRouter>
+      </GlobalProvider>
     </>
   );
 }
