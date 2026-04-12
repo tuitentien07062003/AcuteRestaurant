37d4aba84dc6c41fb93d6f9247bd751e574756b2
tuitentien07062003 <nguyenphanquoctien07062003@gmail.com>
2026-04-12 22:06:49 +0700

    feat: hoàn thành test branch

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.jsx'
-import { GlobalProvider } from "./context/GlobalContext";
+import { GlobalProvider } from "./context/GlobalContext"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 // Create a client for React Query
 const queryClient = new QueryClient({
@@ -21,6 +22,7 @@ createRoot(document.getElementById('root')).render(
       <GlobalProvider>
         <App />
       </GlobalProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 )
