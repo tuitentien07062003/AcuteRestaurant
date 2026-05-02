a18673dba12bdb5e1103cce1e930f3b7d3d8e24b
tuitentien07062003 <nguyenphanquoctien07062003@gmail.com>
2026-05-02 12:35:33 +0700

    g

--- hq_frontend/src/api/forecastApi.js
+++ hq_frontend/src/api/forecastApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/acute/hq",
+  baseURL: "https://acuterestaurant.onrender.com/acute/hq",
   headers: { "Content-Type": "application/json" },
 });
 
